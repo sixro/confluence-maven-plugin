8ade4d60342e51139d2307c57ac41a29a9a217b1
sixro <me@sixro.net>
2014-08-12 10:12:56 +0200

    Refactored packages

---
+++ src/test/java/confluencemavenplugin/ConfluenceMavenPluginTest.java
@@ -0,0 +1,14 @@
+package confluencemavenplugin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfluenceMavenPluginTest {
+
+	@Test
+	public void test() {
+		// FIXME impl
+	}
+
+}
