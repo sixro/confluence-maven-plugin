ccfda9c03c5a67bc59895ff88b69f040fa63fe66
sixro <me@sixro.net>
2014-08-13 12:20:08 +0200

    Now project properties are replaced in resulting HTML

--- src/main/java/confluencemavenplugin/ConfluenceMavenPlugin.java
+++ src/main/java/confluencemavenplugin/ConfluenceMavenPlugin.java
@@ -1,6 +1,12 @@
 package confluencemavenplugin;
 
 import java.io.*;
+import java.util.Collections;
+
+import org.apache.commons.io.*;
+import org.apache.maven.project.MavenProject;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 
 
 /**
@@ -13,12 +19,34 @@ import java.io.*;
  */
 public class ConfluenceMavenPlugin {
 
-	public void generate(File readme, File outputDirectory) throws FileNotFoundException, IOException {
+	public void generate(File file, MavenProject project, File outputDirectory) throws FileNotFoundException, IOException {
 		if (! outputDirectory.exists())
 			outputDirectory.mkdirs();
 		
-		Markdown markdown = new Markdown(readme);
-		markdown.toHtmlFile(outputDirectory);
+		Markdown markdown = new Markdown(file);
+		String html = markdown.toHtml();
+		html = replaceProjectProperties(html, project);
+		
+		String htmlFilename = FilenameUtils.getBaseName(file.getName()) + ".html";
+		File outputFile = new File(outputDirectory, htmlFilename);
+		
+		PrintWriter writer = new PrintWriter(outputFile);
+		writer.write(html);
+		writer.close();
+	}
+
+	private String replaceProjectProperties(String html, MavenProject project) {
+		StringWriter writerOnText = new StringWriter();
+		boolean evaluated = Velocity.evaluate(
+				new VelocityContext(Collections.singletonMap("project", project)), 
+				writerOnText, 
+				getClass().getName(), 
+				html
+		);
+		if (! evaluated)
+			throw new RuntimeException("Unable to replace project properties inside content '" + html + "'");
+		
+		return writerOnText.toString();
 	}
 
 	public void deploy(Confluence confluence, File outputDirectory, String parentTitle) throws DeployException {
--- src/test/resources/README.md
+++ src/test/resources/README.md
@@ -4,6 +4,7 @@ Title
 ## Subtitle
 
 Hello world!
+Project version is ${project.artifactId}.
 
     This is a pre-formatted text
 	And this too
